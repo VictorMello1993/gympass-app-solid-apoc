d7215458a74e7308756d3225fef39954fa5e4c45
Victor Mello <victorsmello93@gmail.com>
2023-05-23 19:25:59 -0300

    create user

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,21 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { registerUseCase } from "../../useCase/register";
+
+export async function register(req: FastifyRequest, res: FastifyReply) {
+  const createUserSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6)
+  });
+
+  const { name, email, password } = createUserSchema.parse(req.body);
+
+  try {
+    await registerUseCase({ name, email, password });
+  } catch (error) {
+    return res.status(409).send();
+  }
+
+  return res.status(201).send();
+};
