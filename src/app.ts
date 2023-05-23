efa129c8e066c7eeded42cd5588c037f609b9ce8
Victor Mello <victorsmello93@gmail.com>
2023-05-22 23:19:48 -0300

    prisma setup and docker configuration

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 export const app = fastify();
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: "Victor Mello",
+    email: "usuario@teste.com.br"
+  }
+})
