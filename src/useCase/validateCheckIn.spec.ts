7ce72f4d35a04f50296787e8626ab5db145356e3
VictorMello1993 <victorsmello93@gmail.com>
2023-06-02 01:20:29 -0300

    validate check-in under 20 minutes rule

--- src/useCase/validateCheckIn.spec.ts
+++ src/useCase/validateCheckIn.spec.ts
@@ -1,7 +1,8 @@
-import { expect, it, describe, beforeEach, afterEach } from "vitest";
+import { expect, it, describe, beforeEach, afterEach, vi } from "vitest";
 import { InMemoryCheckInsRepository } from "../repositories/InMemory/InMemoryCheckInsRepository";
 import { ValidateCheckInUseCase } from "./validateCheckIn";
 import { ResourceNotFoundError } from "./errors/ResourceNotFoundError";
+import { LateCheckInValidationError } from "./errors/LateCheckInValidationError";
 
 let checkInsRepository: InMemoryCheckInsRepository;
 let sut: ValidateCheckInUseCase;
@@ -11,11 +12,11 @@ describe("Validate check-in use case", () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckInUseCase(checkInsRepository);
 
-    // vi.useFakeTimers(); // Antes de executar cada teste, os valores da data serão fictícias
+    vi.useFakeTimers(); // Antes de executar cada teste, os valores da data serão fictícias
   });
 
   afterEach(() => {
-    // vi.useRealTimers(); // Depois de ter rodado cada teste, os valores da data passam ser reais
+    vi.useRealTimers(); // Depois de ter rodado cada teste, os valores da data passam ser reais
   });
 
   it("It should be able to validate check-in", async () => {
@@ -41,5 +42,24 @@ describe("Validate check-in use case", () => {
     await expect(() => sut.execute({
       checkInId: "inexistent-id"
     })).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("It should not be able to validate check-in after 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40)); // 01/01/2023 13:40 - 16:40 em UTC
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01"
+    });
+
+    const TwentyOneMinutesInMs = 1000 * 60 * 21;
+
+    vi.advanceTimersByTime(TwentyOneMinutesInMs); // 01/01/2023 14:01 - 17:01 em UTC
+
+    await expect(() => {
+      sut.execute({
+        checkInId: createdCheckIn.id
+      });
+    }).rejects.toBeInstanceOf(LateCheckInValidationError);
   });
 });
