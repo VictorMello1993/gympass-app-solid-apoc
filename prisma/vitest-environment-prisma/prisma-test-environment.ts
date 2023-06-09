a39dc8033e3e179b59c3f5882898d65aa5622445
Victor Mello <victorsmello93@gmail.com>
2023-06-09 17:33:41 -0300

    test e2e (part 2)

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -3,7 +3,7 @@ import { Environment } from "vitest";
 export default <Environment> {
   name: "prisma",
   async setup() {
-    console.log("Antes de rodar os testes"); // Dentro do escopo de setup() fica o código a ser executado antes de cada teste
+    console.log("Antes de rodar os"); // Dentro do escopo de setup() fica o código a ser executado antes de cada teste
     return {
       async teardown() {
         console.log("Fim dos testes");
