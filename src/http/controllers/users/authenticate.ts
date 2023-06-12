4efc2d7a3bce5253e5e0d18cb1e3eea4f36d05f1
VictorMello1993 <victorsmello93@gmail.com>
2023-06-11 21:15:15 -0300

    refresh token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -22,7 +22,23 @@ export async function authenticate(req: FastifyRequest, res: FastifyReply) {
       }
     });
 
-    return res.status(200).send({ token });
+    const refreshToken = await res.jwtSign({}, {
+      sign: {
+        sub: user.id,
+        expiresIn: "7d"
+      }
+    });
+
+    return res
+      .setCookie("refreshToken", refreshToken, {
+        path: "/",
+        secure: true, // HTTPS
+        sameSite: true,
+        httpOnly: true
+      })
+      .status(200).send({
+        token
+      });
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return res.status(400).send({ message: error.message });
